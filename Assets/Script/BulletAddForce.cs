49c7ad6326e43b199bae57dbd36540f36424a7ef
SinoharaRyuga <raitan3170@gmail.com>
2021-07-14 00:20:25 +0900

    Tankの移動と銃弾を撃てるようにした

---
+++ Assets/Script/BulletAddForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Bulletに力を加えて飛ばすスクリプト
+/// </summary>
+public class BulletAddForce : MonoBehaviour
+{
+    /// <summary>弾の生存期間（秒)</summary>
+    [SerializeField] float m_lifeTime = 5f;
+    /// <summary>弾が飛ぶ速さ</summary>
+    [SerializeField] float m_speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        Rigidbody  rb = GetComponent<Rigidbody>();
+        //z方向に飛ばす
+        rb.AddForce(transform.forward * m_speed, ForceMode.Impulse);
+        // m_lifeTime分時間が経過したら弾丸を削除する
+        Destroy(this.gameObject, m_lifeTime);
+        gameObject.transform.parent = null;
+    }
+}
---
+++ Assets/Script/TankController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+     Rigidbody m_rb = default;
+    [SerializeField] Transform m_bulletSpwan;
+    [SerializeField] GameObject m_bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(m_bullet, m_bulletSpwan);
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_rb.velocity = transform.forward * 10;
+            Debug.Log("W");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            m_rb.velocity = transform.forward * -10;
+            Debug.Log("S");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }  
+    }
+}
