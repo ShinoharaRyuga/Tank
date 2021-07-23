2c3f39cd1104775e016a3e339ae14e0c87451bf3
SinoharaRyuga <raitan3170@gmail.com>
2021-07-23 20:07:33 +0900

    Playerをゲームコントローラーで操作できるようにした

--- Assets/Script/TankController.cs
+++ Assets/Script/TankController.cs
@@ -8,40 +8,59 @@ public class TankController : MonoBehaviour
     [SerializeField] Transform m_bulletSpwan;
     [SerializeField] GameObject m_bullet;
     float m_speed = 8f;
+    private AudioSource m_audio;
+    [SerializeField] AudioClip m_sound;
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
+        m_audio = this.gameObject.AddComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Controller();
+       
     }
 
     private void Controller()
     {
-        if (Input.GetKey(KeyCode.W))
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log("hは" + h);
+        Debug.Log("vは" + v);
+
+        
+        if(v == 0)
         {
-            m_rb.velocity = transform.forward * m_speed;
+            m_rb.velocity = Vector3.zero;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else
         {
-            m_rb.velocity = transform.forward * -m_speed;
+            if (v > 0.2)
+            {
+                m_rb.velocity = transform.forward * m_speed;
+            }
+            else if (v == -1)
+            {
+                m_rb.velocity = transform.forward * -m_speed;
+            }
         }
-        if (Input.GetKey(KeyCode.D))
+        if (h > 0)
         {
-            transform.Rotate(0, 0.5f, 0);
+            transform.Rotate(0, 1f, 0);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (h < 0)
         {
-            transform.Rotate(0, -0.5f, 0);
+            transform.Rotate(0, -1f, 0);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             Instantiate(m_bullet, m_bulletSpwan);
+            m_audio.PlayOneShot(m_sound);
+            Debug.Log("発射！！");
         }
     }
 }
