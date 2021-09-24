d1ee667a28745997555e9f7cc24d396f5106abeb
SinoharaRyuga <raitan3170@gmail.com>
2021-09-25 08:10:31 +0900

    新しい障害物、敵戦車の作成

---
+++ Assets/Script/DangerousScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerousScript : MonoBehaviour
+{
+    [SerializeField] GameManager m_gameManager = default;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerTank")
+        {
+            m_gameManager.m_failFlag = true;
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
